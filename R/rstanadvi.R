8bda35b77ea6fabff1c012f9b35bea776ae188dc
Kieran Campbell <kieranrcampbell@gmail.com>
2016-02-03 15:50:43 +0000

    updated paths

--- R/rstanadvi.R
+++ R/rstanadvi.R
@@ -33,6 +33,7 @@ stan_rdump_list <- function(l, file) {
 #' @param data_file_name Name of intermediate file to which data is dumped (optional).
 #' @param init_file_name Name of intermediate file to which initial values are dumped (optional).
 #' @param output_file_name Name of output file (optional).
+#' @param verbose Print extra output
 #' 
 #' @import rstan
 #' @importFrom readr read_csv
@@ -43,22 +44,36 @@ stan_rdump_list <- function(l, file) {
 rstanadvi <- function(stan_model_name, data, path_to_stanmodel = ".", init = NULL,
                              path_to_cmdstan = "../cmdstan-2.9.0",
                              data_file_name = "data.R", init_file_name = "init.R",
-                             output_file_name = paste0(stan_model_name, ".txt")) {
+                             output_file_name = paste0(stan_model_name, ".txt"),
+                              verbose = FALSE) {
   # dump data to file
   stan_rdump_list(data, data_file_name)
   if(!is.null(init)) stan_rdump_list(init, init_file_name)
   
   stan_model_name <- gsub(".stan", "", stan_model_name, fixed = TRUE)
   
+  model_file_name <- file.path(path_to_stanmodel, paste0(stan_model_name, ".stan"))
+  makefile_name <- file.path(path_to_cmdstan, "Makefile")
+  
+  if(!file.exists(model_file_name)) stop(paste("Stan model", model_file_name, "not found"))
+  if(!file.exists(makefile_name)) stop("Makefile not found in cmdStan implementation")
+  
   # construct commands
-  make_cmd <- paste0("cd ", path_to_cmdstan, "; make ", file.path(path_to_stanmodel, stan_model_name))
+  curr_dir <- getwd()
+  
+  make_cmd <- paste0("cd ", path_to_cmdstan, "; make ", file.path(curr_dir, path_to_stanmodel, stan_model_name))
   run_cmd <- paste0("./", stan_model_name, " variational data file=", data_file_name)
   if(!is.null(init)) run_cmd <- paste0(run_cmd, " init=", init_file_name)
   run_cmd <- paste0(run_cmd, " output file=", output_file_name)
   
   # run commands
-  system(make_cmd)
-  system(run_cmd)
+  if(verbose) message(make_cmd)
+  mres <- system(make_cmd)
+  if(mres != 0) stop("Error building stan model")
+  
+  if(verbose) message(run_cmd)
+  rres <- system(run_cmd)
+  if(rres != 0) stop("Error in ADVI on stan model")
   
   return( read_csv(output_file_name, comment="#") )
 }
